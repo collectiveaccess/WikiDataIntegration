2f70f09414328b3842f29eadf21cb4f06f740811
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2022-06-07 19:02:51 -0500

    add linter

---
+++ linter.py
@@ -0,0 +1,15 @@
+import subprocess
+
+
+def lint():
+    _exec("black ./scripts")
+    _exec("flake8 ./scripts")
+
+
+def _exec(command):
+    process = subprocess.Popen(command.split())
+    output, error = process.communicate()
+
+
+if __name__ == "__main__":
+    lint()
