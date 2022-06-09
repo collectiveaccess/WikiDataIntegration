0aaf37930ac0c30ea08ede87b8a606ee8a37922b
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2022-06-08 20:47:24 -0500

    search dancing digital data

---
+++ scripts/dancing_digital.py
@@ -0,0 +1,27 @@
+import sys
+from pathlib import Path
+import csv
+
+import pywikibot
+import wikidata_utils as wd
+
+data_path = Path(__file__).resolve().parent.parent / "data"
+sys.path.append(str(data_path))
+
+site = pywikibot.Site("wikidata", "wikidata")
+
+
+with open(data_path / "venues.csv", mode="r") as file:
+    csvFile = csv.DictReader(file)
+    for line in csvFile:
+        label = line["name"]
+        res = wd.item_exists(site, label)
+
+        print("----")
+        print(line)
+        if len(res) == 0:
+            print("zero items found")
+        else:
+            print(f"{len(res)} items found")
+            for r in res:
+                print(r)
