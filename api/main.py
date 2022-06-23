8ca24ec5aefb0e3bdf9468cdc3e99f75b7e25c48
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2022-06-23 16:33:20 -0700

    fix - add headers so api returns utf-8

--- api/main.py
+++ api/main.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI, HTTPException
 import sys
 from pathlib import Path
-import pywikibot
 import re
+
+import pywikibot
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
 
 project_path = Path(__file__).resolve().parent.parent
 sys.path.append(str(project_path))
@@ -10,13 +12,16 @@ sys.path.append(str(project_path / "scripts"))
 
 import scripts.utils.wikidata_utils as wd
 
-
 app = FastAPI()
+
+# need to set headers to ensure we are using utf-8
+headers = {"Content-Type": "application/json;charset=UTF-8", "Charset": "utf-8"}
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    content = {"Hello": "World", "message": "塔当"}
+    return JSONResponse(content=content, headers=headers)
 
 
 @app.get("/items/{item_id}")
@@ -25,24 +30,24 @@ def read_item(item_id):
     if not re.search(r"^Q[0-9]+$", item_id):
         raise HTTPException(status_code=404, detail="Item not found")
 
-    site = pywikibot.Site("test", "wikidata")
+    site = pywikibot.Site("wikidata", "wikidata")
     repo = site.data_repository()
     item = pywikibot.ItemPage(repo, item_id)
 
     if item.exists():
-        results = wd.format_display_item(item)
+        content = wd.format_display_item(item)
     else:
         raise HTTPException(status_code=404, detail="Item not found")
 
-    return results
+    return JSONResponse(content=content, headers=headers)
 
 
 @app.get("/search")
 def read_search(keyword=None):
     site = pywikibot.Site("wikidata", "wikidata")
     if keyword:
-        result = wd.item_exists(site, keyword)
+        content = wd.item_exists(site, keyword)
     else:
-        result = []
+        content = []
 
-    return result
+    return JSONResponse(content=content, headers=headers)
