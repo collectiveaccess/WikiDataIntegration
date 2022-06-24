46d50e9312b50d9bad68c463cf4de20f4e979614
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2022-06-23 19:05:08 -0700

    add script to download wikidata.org properties

---
+++ scripts/download_wikidata_labels.py
@@ -0,0 +1,30 @@
+import sys
+from pathlib import Path
+import re
+from datetime import date
+from os.path import exists
+import json
+import os
+
+import utils.wikidata_utils as wd
+
+project_path = Path(__file__).resolve().parent.parent
+sys.path.append(str(project_path))
+
+today = date.today()
+filepath = project_path / "data" / f"wikdata_labels_{today}.json"
+
+# if file exists, do nothing
+if exists(filepath):
+    print("wikidata.org labels were not updated.")
+# if file does not exist, delete old files, get properties, create new file
+else:
+    for path in Path(project_path / "data").glob("wikdata_labels*.json"):
+        os.remove(path)
+
+    properties = wd.fetch_all_wikidata_properties()
+    with open(filepath, "w") as outfile:
+        json_object = json.dumps(properties, indent=2)
+        outfile.write(json_object)
+
+    print("wikidata.org labels were updated.")
