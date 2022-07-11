0da37368c2c44a8b18c5a1044148164ef9ee5523
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2022-07-10 19:16:16 -0700

    add method to  import wikidata item to local wikibase

--- __init__.py
+++ __init__.py
@@ -7,6 +7,10 @@ from .scripts.utils.wiki_queries import (
     search_keyword,
     fetch_and_format_all_properties,
     fetch_and_format_external_id_properties,
+    fetch_and_format_labels_for_ids_sqarql,
+    fetch_and_format_labels_for_ids
 )
 from .scripts.utils.wikidata_utils import *
 from .scripts.utils.logger import logger
+from .scripts.utils.wiki_serialization import get_claim_label
+from .scripts.utils.import_wikidata_records import import_wikidata_item_to_local_wikibase
---
+++ scripts/utils/import_wikidata_records.py
@@ -0,0 +1,230 @@
+import pywikibot
+
+import wikidata_utils as wd
+from scripts.utils.logger import logger
+import scripts.utils.wiki_queries as wq
+import scripts.utils.wiki_serialization as ws
+
+
+def find_or_create_local_item(item_dict, local_site, local_repo):
+    lang = ws.get_claim_language(item_dict)
+    label = item_dict["labels"][lang]
+    description = item_dict["descriptions"][lang]
+
+    # check if item exists locally
+    existing = False
+    existing_id = None
+    results = wd.search_keyword(local_site, label, lang)
+    for result in results:
+        # if no description, check if any of the existing records have same
+        # label as the current row
+        if not description:
+            if result["label"] == label:
+                existing = True
+                existing_id = result["id"]
+
+        # check if any of the existing records have same description and
+        # label as current row
+        elif "label" in result and result["label"]:
+            if result["description"] == description and result["label"] == label:
+                existing = True
+                existing_id = result["id"]
+
+    # get existing item
+    if existing:
+        item = pywikibot.ItemPage(local_repo, existing_id)
+    # create new item
+    else:
+        item = wd.import_item(local_site, item_dict, import_sitelinks=False)
+
+    return item
+
+
+def add_statements_to_local_item(item_dict, repo, local_item, local_site, local_repo):
+    # iterate over all the wikidata.org claims
+    for property, values in item_dict["claims"].items():
+        for claim in values:
+
+            new_claim_value = wd.convert_to_local_claim_value(
+                local_site, local_repo, claim, import_sitelinks=False
+            )
+            if new_claim_value:
+                wd.add_claim(repo, local_item, property, new_claim_value)
+
+
+def add_qualifier_to_wikidata_claim(
+    claim, local_claim, local_site, local_repo, site, repo, import_sitelinks
+):
+    # claim.qualifiers returns an ordered dictionary
+    for qualifier_property, qualifier_claims in claim.qualifiers.items():
+
+        for qualifier_claim in qualifier_claims:
+            qualifier_value = wd.convert_to_local_claim_value(
+                local_site,
+                local_repo,
+                qualifier_claim,
+                import_sitelinks,
+            )
+
+            # check is source exists locally
+            exists = False
+            if qualifier_property in local_claim.qualifiers:
+                for l_source in local_claim.qualifiers[qualifier_property]:
+                    if l_source.getTarget() == qualifier_value:
+                        exists = True
+
+            # add new source claim
+            if not exists:
+                try:
+                    new_claim = pywikibot.Claim(repo, qualifier_property)
+                    new_claim.setTarget(qualifier_value)
+                    local_claim.addQualifier(new_claim, summary="Add qualifier.")
+                    logger.info(
+                        f"Add qualifier: {claim.id} "
+                        f"{qualifier_property} {qualifier_value}"
+                    )
+                except:
+                    logger.error(
+                        f"Qualifier not added: {claim.id} "
+                        f"{qualifier_property} {qualifier_value}"
+                    )
+
+
+def add_source_to_wikidata_claim(
+    claim, local_claim, local_site, local_repo, site, repo, import_sitelinks
+):
+    # claim.sources returns a list or ordered dictionariers
+    for claim_source in claim.sources:
+        new_sources = []
+        for source_property, source_values in claim_source.items():
+
+            # NOTE:skip P4656 "Wikimedia import URL"
+            # since we don't have wikipedia pages.
+            # https://phabricator.wikimedia.org/T301243
+            if source_property in ["P4656"]:
+                continue
+            # NOTE: skip reference url since there is a bug with saving URL
+            if source_property in ["P854"]:
+                continue
+
+            for source_claim in source_values:
+                source_value = wd.convert_to_local_claim_value(
+                    local_site,
+                    local_repo,
+                    source_claim,
+                    import_sitelinks,
+                )
+
+                # check is source exists locally
+                exists = False
+                for l_source in local_claim.sources:
+                    if source_property in l_source.keys():
+                        for ll_source in l_source[source_property]:
+                            if ll_source.getTarget() == source_value:
+                                exists = True
+
+                # add new source claim
+                if not exists:
+                    try:
+                        new_source = pywikibot.Claim(repo, source_property)
+                        new_source.setTarget(source_value)
+                    except:
+                        print("new_source target", source_property)
+                    new_sources.append(new_source)
+
+        if len(new_sources) > 0:
+            try:
+                local_claim.addSources(new_sources, summary="Adding sources.")
+                logger.info(f"Sources added: {local_claim.id}")
+            except:
+                logger.error(f"Sources not saved: {local_claim.id}")
+
+
+def create_local_id_label_dictionary(local_item):
+    # can't use sparql to get labels for local wikibase qid because I can't
+    # get sparql working. use api call to local wikibase to get labels.
+    # api call doesn't work on federated properties.
+    qids = wd.get_ids_for_item(
+        local_item, local_item.toJSON(), include_pids=False, include_qids=True
+    )
+    qid_dict = wq.fetch_and_format_labels_for_ids(qids, "http://whirl.mine.nu:8888")
+
+    # use sparql query to wikidata.org to get pids
+    pids = wd.get_ids_for_item(
+        local_item, local_item.toJSON(), include_pids=True, include_qids=False
+    )
+    pid_dict = wq.fetch_and_format_labels_for_ids_sqarql(pids)
+
+    return {**qid_dict, **pid_dict}
+
+
+def add_sources_and_qualifiers_to_local_item(
+    item_dict,
+    id_label_dict,
+    site,
+    repo,
+    local_item,
+    local_id_label_dict,
+    local_site,
+    local_repo,
+):
+    # iterate over all the wikidata.org claims
+    for property, claims in item_dict["claims"].items():
+        for claim in claims:
+            # skip commonsMedia claims since we don't import them
+            if claim.type == "commonsMedia":
+                continue
+
+            # get the corresponding local claim
+            if claim.id in local_item.claims:
+                for l_claim in local_item.claims[claim.id]:
+                    if ws.get_claim_label(
+                        l_claim, local_id_label_dict, False
+                    ) == ws.get_claim_label(claim, id_label_dict, False):
+                        local_claim = l_claim
+
+            add_source_to_wikidata_claim(
+                claim,
+                local_claim,
+                local_site,
+                local_repo,
+                site,
+                repo,
+                import_sitelinks=False,
+            )
+            add_qualifier_to_wikidata_claim(
+                claim,
+                local_claim,
+                local_site,
+                local_repo,
+                site,
+                repo,
+                import_sitelinks=False,
+            )
+
+
+def import_wikidata_item_to_local_wikibase(qid, site, local_site):
+    pywikibot.config.put_throttle = 1
+    local_repo = local_site.data_repository()
+    print(local_site.throttle.writedelay)
+
+    repo = site.data_repository()
+    item = pywikibot.ItemPage(repo, qid)
+    item_dict = item.get()
+
+    local_item = find_or_create_local_item(item_dict, local_site, local_repo)
+    add_statements_to_local_item(local_item, item_dict, repo, local_site, local_repo)
+
+    id_label_dict = wd.create_id_label_dictionary(item, item.toJSON())
+    local_id_label_dict = create_local_id_label_dictionary(local_item)
+
+    add_sources_and_qualifiers_to_local_item(
+        item_dict,
+        id_label_dict,
+        site,
+        repo,
+        local_item,
+        local_id_label_dict,
+        local_site,
+        local_repo,
+    )
